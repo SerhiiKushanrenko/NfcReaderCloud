afd484c2a6b14fb1a5b561b87b7844b9cb827040
SerhiiKushanrenko <Serhii.Kushnarenko@fivesysdev.com>
2022-12-02 15:43:45 +0200

    add status controller

---
+++ NfcReaderCloud/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NfcReaderCloud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            return Ok();
+        }
+    }
+}
